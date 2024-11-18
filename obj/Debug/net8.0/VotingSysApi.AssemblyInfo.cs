af2618c9aa4ad713159294269f29ec308b976f17
thomaswei <thomaswei@mail.apex.com.tw>
2024-11-18 15:37:24 +0800

    update web

--- obj/Debug/net8.0/VotingSysApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/VotingSysApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VotingSysApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9613de8703a80decd0fccc3d67bc5d05afcfdc9")]
 [assembly: System.Reflection.AssemblyProductAttribute("VotingSysApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VotingSysApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

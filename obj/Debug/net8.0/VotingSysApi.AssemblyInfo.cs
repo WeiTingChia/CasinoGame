4e11aef517e87cf4cd7c812f4f5bd121c567d64b
thomaswei <thomaswei@mail.apex.com.tw>
2024-11-21 10:23:41 +0800

    update yml file

--- obj/Debug/net8.0/VotingSysApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/VotingSysApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VotingSysApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c40868946e89aab7ab6f8f629b28738d82babbd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+545d6f24876ce5f57cc93b308a3256dd2492a7eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("VotingSysApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VotingSysApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

65f1014762db8df7cc987eba87bf11efec3ecb33
thomaswei <thomaswei@mail.apex.com.tw>
2025-02-19 11:12:29 +0800

    update ui

--- Program.cs
+++ Program.cs
@@ -29,10 +29,11 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseRouting();
+
 app.UseAuthentication();
 app.UseAuthorization();
 
-app.UseRouting();
 
 app.UseDefaultFiles();
 app.UseStaticFiles();
--- obj/Debug/net8.0/VotingSysApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/VotingSysApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VotingSysApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65f10691d61c6076c989b436d274b915c5600418")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2875a80ec31eddd3cfd66ae95651fe407f6dc0d")]
 [assembly: System.Reflection.AssemblyProductAttribute("VotingSysApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VotingSysApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
